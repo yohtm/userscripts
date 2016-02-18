1e8661e77eb87701de7feae3346b29f22c40d903
Yohan <yohan@realisations.net>
2016-02-18 11:15:06 -0500

    Gantt clickable milestones

---
+++ Teamwork Real Gantt.user.js
@@ -0,0 +1,21 @@
+
+// ==UserScript==
+// @name       Teamwork Real Gantt
+// @namespace  http://realisations.net
+// @version    0.1
+// @description  Make the world a better place
+// @match      https://realisationsnet.teamworkpm.net/projects?display=projectChart
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+function run(){
+    
+    imgs = $("img[id^='m_i_']").each(function(){
+        mid = $(this).attr("id").split("_")[2]
+        $(this).parent().html("<a href='milestones/" + mid + "'>" + $(this).parent().html() + "</a>")
+    })
+}
+
+$(document).ready(function() {    
+    setTimeout(function() { run(); }, 1200); 
+});
