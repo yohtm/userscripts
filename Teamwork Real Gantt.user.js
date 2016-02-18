cc5e891acdc9c5cfaf97bcf95c98f0f3f9a83abb
Yohan <yohan@realisations.net>
2016-02-18 12:12:58 -0500

    v0.2

--- Teamwork Real Gantt.user.js
+++ Teamwork Real Gantt.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name       Teamwork Real Gantt
 // @namespace  http://realisations.net
-// @version    0.1
+// @version    0.2
 // @description  Make the world a better place
 // @match      https://realisationsnet.teamworkpm.net/*
 // @require http://code.jquery.com/jquery-latest.js
